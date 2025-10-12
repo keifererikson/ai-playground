771e4517c73523ecd23e1e9f9edf01935e545c08
Keifer Erikson <keifer@erikson.dev>
2025-10-12 10:42:48 -0600

    feat: add llm_manager initialization on launch

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,12 @@
-from typing import Union
-
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from ai.llm_manager import LLMManager
+
+load_dotenv()
 
 app = FastAPI()
+
+llm_manager = LLMManager()
 
 
 @app.get("/")
