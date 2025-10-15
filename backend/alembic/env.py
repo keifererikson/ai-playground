477dd9564f16354ed7c4bbe6b5b43548a91f1c7f
Keifer Erikson <keifer@erikson.dev>
2025-10-15 14:27:59 -0600

    fix: alembic not loading .env

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -7,11 +7,10 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from app.database import Base
-from app.models import Settings
-
 load_dotenv()
 
+from app.database import Base
+from app.models import Settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
